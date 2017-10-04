fa7ebda3a1c63889e487e9ee1128ad738cba7538
Claudia Zeledon <cjzeledon@gmail.com>
2017-10-04 11:24:21 -0400

    Fixed LotBotController.java

--- src/main/java/com/theironyard/charlotte/lotbot/LotBotController.java
+++ src/main/java/com/theironyard/charlotte/lotbot/LotBotController.java
@@ -3,8 +3,8 @@ package com.theironyard.charlotte.lotbot;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.*;
 
 /*
 While the traditional MVC controller (@Controller) relies on the View technology, the RESTful web service controller
@@ -14,27 +14,15 @@ While the traditional MVC controller (@Controller) relies on the View technology
 @RestController
 public class LotBotController {
 
-    // Here we're storing a list of lots.
-    // does a list make sense here? It may,
-    // but it's also possible another data
-    // structure will make your life easier.
-    // think critically about your choices
     List<Lot> lots = new ArrayList<>();
 
-    /**
-     * The @PostConstruct method will cause whichever
-     * method it's annotating to run after the controller
-     * is created by Spring.
-     *
-     * In this case, let's use it to give a value to
-     * our list of "lots"
-     */
+    List<Transaction> bills = new ArrayList<>();
+
+
     @PostConstruct
     public void postConstruct() {
-        // we have to create lots of lots here
-        // if you want fewer lots than 20
-        // that's okay too
-        for (int i = 0;i < 3;i++) {
+        // Create number of lots
+        for (int i = 0; i < 4; i++) {
             lots.add(Lot.createLot());
         }
     }
@@ -42,43 +30,61 @@ public class LotBotController {
     //Get a list of all lots in the system, including # of spots
     @CrossOrigin
     @RequestMapping(path = "/lots", method = RequestMethod.GET)
-    public List<Lot> getListofLots(){
+    public List<Lot> getAllLots() {
         return lots;
     }
 
-
-    /*
-    GET /lots/<id>
-    Get a list of the status of all spots in the specified lot, including the license plate # of anyone parked
-    */
+    //Get a list of the status of all spots in the specified lot, including the license plate # of anyone parked
     @CrossOrigin
     @RequestMapping(path = "/lots/{id}", method = RequestMethod.GET)
-    public Lot getLotID(@PathVariable("id") int id){
+    public Lot getLotID (@PathVariable("id") int id){
         return lots.get(id);
     }
 
-    /*
-    POST /lots/<id>
-    Park a new car in the specified lot. Must send the Car object in the request body
-     */
+    //Park a new car in the specified lot. Must send the Car object in the request body
     @CrossOrigin
-    @RequestMapping(path = "/lots/{id}", method = RequestMethod.POST)
-    public void parkAVehicle (@PathVariable("id") int id,
-                             @RequestBody Vehicle car){
-        lots.get(id);
+    @RequestMapping(path = "/lots/{id}/{spotId}", method = RequestMethod.POST)
+    public void spacePark (@PathVariable("id") int id,
+                           @PathVariable("spotId") int spotId,
+                           @RequestBody Vehicle vehicle){
+
+        Transaction trans = new Transaction();
+        trans.setCheckInDate(LocalDateTime.now());
+        trans.setVehicle(vehicle);
+
+        lots.get(id).getSpaces()[spotId] = new Space(trans);
+
+        bills.add(trans);
     }
 
-    /*
-    PUT /lots/<id>/<spot>
-    Open up the specified spot and return the total owed
-     */
+    //Open up the specified spot and return the total owed
+    @CrossOrigin
+    @RequestMapping(path = "/lots/{id}/{spotId}", method = RequestMethod.PUT)
+    public double spaceLeave (@PathVariable("id") int id,
+                              @PathVariable("spotId") int spotId){
 
+        Transaction currentTrans = lots.get(id).getSpaces()[spotId].getTrans();
 
-    /*
-    GET /transactions
-    Return a list of all transactions, along with the bill and license late number of the charged vehicle
-     */
+        currentTrans.setCheckOutDate(LocalDateTime.now());
 
+        Double amountDiff = DateHelper.getHoursBetweenDates(currentTrans.getCheckInDate(), currentTrans.getCheckOutDate());
+
+        currentTrans.setPrice(amountDiff * Math.random() * 15);
+
+        bills.add(currentTrans);
+
+        lots.get(id).getSpaces()[spotId].setTrans(null);
+
+        return Double.parseDouble(String.valueOf(currentTrans.getPrice()));
+    }
+
+    //Return a list of all transactions, along with the bill and license late number of the charged vehicle
+    @CrossOrigin
+    @RequestMapping(path ="/transaction", method = RequestMethod.GET)
+    public List<Transaction> transList(){
+        return bills;
+    }
 
 
 }
+
